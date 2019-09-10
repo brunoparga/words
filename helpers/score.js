7c8068f4e5f64c1c14b581d6f6801b12dd6ad07e
Bruno Parga <brunoparga@gmail.com>
2019-09-10 10:50:19 -0300

    Score words based on Scrabble letter values

--- helpers/score.js
+++ helpers/score.js
@@ -1,6 +1,7 @@
-const fs = require('fs');
+const fs = require('fs')
 
 const wordInGrid = require('./in_grid')
+const values = require('./values')
 
 const englishWord = (word) => {
   const allWords = fs.readFileSync('words.txt', 'utf8')
@@ -9,7 +10,7 @@ const englishWord = (word) => {
 
 const calc = (word, grid) => {
   if (!wordInGrid(word, grid) || !englishWord(word)) { return 0 }
-  return word.length * 100
+  return word.split('').reduce(values, 0) * 100
 }
 
 module.exports = calc
