784853ab0c51ea3a8c124ffea3800450527a4071
Bruno Parga <brunoparga@gmail.com>
2019-09-10 11:50:55 -0300

    Generate grid from Scrabble tiles

--- helpers/letters.js
+++ helpers/letters.js
@@ -1,20 +1,47 @@
 const _ = require('lodash')
 
+const generateTiles = () => {
+  const tiles = []
+
+  const tileCount = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1
+  }
+
+  for (const letter in tileCount) {
+    _.times(tileCount[letter], () => tiles.push(letter))
+  }
+
+  return tiles
+}
+
 const generateLetters = () => {
-  const letters = []
-
-  for (let i = 0; i < 2; i++) {
-    const vowels = 'AEIOU'.split('')
-    const index = Math.floor(Math.random() * vowels.length)
-    letters.push(vowels[index])
-  }
-
-  for (let j = 0; j < 7; j++) {
-    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')
-    const index = Math.floor(Math.random() * alphabet.length)
-    letters.push(alphabet[index])
-  }
-
+  const tiles = generateTiles()
+  const letters = Array(8).fill(undefined).map(() => _.sample(tiles))
   return _.shuffle(letters)
 }
 
