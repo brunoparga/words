6cc45d32a4695fd5742fe7f46a3ce6c9a74d2bc1
Bruno Parga <brunoparga@gmail.com>
2019-09-21 17:28:30 -0300

    Put dictionary and letter data in folder

--- helpers/letters.js
+++ helpers/letters.js
@@ -1,44 +1,19 @@
+const fs = require('fs');
+const mapValues = require('lodash/mapValues');
 const times = require('lodash/times');
 const difference = require('lodash/difference');
 const sample = require('lodash/sample');
 const shuffle = require('lodash/shuffle');
 
 const generateTiles = () => {
-  const tileCount = {
-    A: 9,
-    B: 2,
-    C: 2,
-    D: 4,
-    E: 12,
-    F: 2,
-    G: 3,
-    H: 2,
-    I: 9,
-    J: 1,
-    K: 1,
-    L: 4,
-    M: 2,
-    N: 6,
-    O: 8,
-    P: 2,
-    Q: 1,
-    R: 6,
-    S: 4,
-    T: 6,
-    U: 4,
-    V: 2,
-    W: 2,
-    X: 1,
-    Y: 2,
-    Z: 1
-  }
-
-  for (const letter in tileCount) {
-    times(tileCount[letter], () => tiles.push(letter))
-  }
-
   const tiles = [];
 
+  let tileCount = JSON.parse(fs.readFileSync('data/letters.json'));
+  tileCount = mapValues(tileCount, 'count');
+
+  Object.keys(tileCount).forEach((letter) => {
+    times(tileCount[letter], () => tiles.push(letter));
+  });
 
   return tiles;
 };
