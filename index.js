71af588e8301109285640c2a53bd79e0e18f38fc
Bruno Parga <brunoparga@gmail.com>
2019-11-02 17:40:36 -0300

    Prepare for Heroku deployment

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-// Access file paths
 const path = require('path');
-
 const express = require('express');
-// Parse POST request body
 const bodyParser = require('body-parser');
 
 const routes = require('./routes/routes');
@@ -10,7 +7,6 @@ const routes = require('./routes/routes');
 const app = express();
 app.set('view engine', 'pug');
 
-// Middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
@@ -18,5 +14,4 @@ app.use((_req, res) => {
   res.status(404).render('404', { title: 'Page Not Found!' });
 });
 
-// App listens
 app.listen(3000);
--- test/in_grid.js
+++ test/in_grid.js
@@ -1,21 +1,21 @@
 const should = require('chai').should();
 const inGrid = require('../helpers/in_grid');
 
-describe('wordInGrid', function () {
-  it('should return true for an empty word', function () {
+describe('wordInGrid', function() {
+  it('should return true for an empty word', () => {
     const testValue = inGrid('', 'BANANA'.split(''));
     testValue.should.equal(true);
   });
-  it('should return false for an empty grid', function () {
+  it('should return false for an empty grid', () => {
     const testValue = inGrid('BANANA', []);
-    testValue.should.equal(false)
+    testValue.should.equal(false);
   });
-  it('should return false if the grid does not contain the word', function () {
-    const testValue = inGrid('BANANA', 'PINEAPPLE'.split(''))
-    testValue.should.equal(false)
+  it('should return false if the grid does not contain the word', () => {
+    const testValue = inGrid('BANANA', 'PINEAPPLE'.split(''));
+    testValue.should.equal(false);
   });
-  it('should return true if the grid contains the word', function () {
-    const testValue = inGrid('BANANA', 'ABCADNAANE'.split(''))
-    testValue.should.equal(true)
+  it('should return true if the grid contains the word', () => {
+    const testValue = inGrid('BANANA', 'ABCADNAANE'.split(''));
+    testValue.should.equal(true);
   });
 });
