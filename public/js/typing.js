3d1791e0caa8933de1ea68d88369be0c7a918951
Bruno Parga <brunoparga@gmail.com>
2019-11-02 22:35:41 -0300

    Focus on input only if typed key is a letter

--- public/js/typing.js
+++ public/js/typing.js
@@ -1,8 +1,10 @@
 const input = document.querySelector('input');
 
 window.addEventListener('keyup', (event) => {
-  if (document.activeElement !== input) {
-    console.log(event);
+  const inputNotFocused = document.activeElement !== input;
+  const keyIsLetter = event.key.length === 1
+    && event.key.toUpperCase() !== event.key.toLowerCase();
+  if (inputNotFocused && keyIsLetter) {
     input.value = event.key;
     input.focus();
   }
