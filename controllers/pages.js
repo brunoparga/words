d66f6cd03c822e0ff25289d06d6c8ff1a314a0b7
Bruno Parga <brunoparga@gmail.com>
2019-11-02 21:09:19 -0300

    Change page titles and footer

--- controllers/pages.js
+++ controllers/pages.js
@@ -2,7 +2,7 @@ const letters = require('../helpers/letters');
 const scoreCalc = require('../helpers/score');
 
 exports.getLetters = (_req, res) => {
-  res.render('letters', { letters: letters(), title: 'Make your Word' });
+  res.render('letters', { letters: letters(), title: 'Make a word' });
 };
 
 exports.postWord = (req, res) => {
@@ -10,7 +10,7 @@ exports.postWord = (req, res) => {
   const grid = req.body.letters.replace(/\W/gi, '');
   const score = scoreCalc(word.toUpperCase(), grid.split(''));
   const assigns = {
-    word, grid, score, title: "Here's your score",
+    word, grid, score, title: 'Score',
   };
   res.render('word', assigns);
 };
