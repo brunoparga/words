8a1dd88664093d013f2a4b9e286d78439ca3d40d
Bruno Parga <brunoparga@gmail.com>
2019-11-02 22:27:19 -0300

    Adjust style

--- public/js/typing.js
+++ public/js/typing.js
@@ -2,8 +2,8 @@ const input = document.querySelector('input');
 
 window.addEventListener('keyup', (event) => {
   if (document.activeElement !== input) {
-    console.log(event)
-    input.value = event.key
-    input.focus()
+    console.log(event);
+    input.value = event.key;
+    input.focus();
   }
 });
