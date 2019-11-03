802335caaf4a70b6b11689970899c1f8e320e169
Bruno Parga <brunoparga@gmail.com>
2019-11-02 22:24:38 -0300

    Allow user to simply start typing
    To make for a better UX, instead of requiring the user to click on the
    input box every time ey want to enter a word, the box now autofocuses as
    soon as the first letter is typed.

---
+++ public/js/typing.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('input');
+
+window.addEventListener('keyup', (event) => {
+  if (document.activeElement !== input) {
+    console.log(event)
+    input.value = event.key
+    input.focus()
+  }
+});
